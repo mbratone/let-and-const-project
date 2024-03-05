35d7ac0907e5940c1882480bcf22f7d297365c4d
mbratone <159495107+mbratone@users.noreply.github.com>
2024-03-04 20:07:53 -0500

    Add files via upload

---
+++ let and const exercise/app.js
@@ -0,0 +1,9 @@
+const PI =3.14;
+
+// var is function-scoped so it is only applicable to the function it is declared in, while let block-scoped so it is confined within the block its delcared.
+
+// var can redclared const cannot. 
+
+// let can be reassigned but const will always be the same. it cannot be delcared or reassigned. 
+
+// hoisting is when JS variables and function declarations are moved to the top of their containing scope. 
